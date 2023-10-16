951ed08bed43644ee874e13d0c46179183b5e21d
N Cee Dee <nnajidavid58@gmail.com>
2023-10-17 00:07:36 +0100

    updated

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,32 +1,31 @@
-const cancel = document.querySelector(".cancel");
-const bar = document.querySelector(".bar");
-const mobileNav = document.querySelector( ".mobile-nav" );
+  const cancel = document.querySelector(".cancel");
+  const bar = document.querySelector(".bar");
+  const mobileNav = document.querySelector(".mobile-nav");
 
-bar.addEventListener( "click", () =>
-{
-    mobileNav.style.display="block"
-})
+  bar.addEventListener("click", () => {
+    mobileNav.style.display = "block";
+  });
 
-cancel.addEventListener("click", () => {
-  mobileNav.style.display = "none";
-});
+  cancel.addEventListener("click", () => {
+    mobileNav.style.display = "none";
+  });
 
+  const dropdownToggle = document.querySelector(".sect");
+  const dropdownContent = document.querySelector(".mobile-dropdown-content");
+  const chevronIcon = document.querySelector(".arrow-down");
 
-const dropdownToggle = document.querySelector(".sect");
-const dropdownContent = document.querySelector(".mobile-dropdown-content");
-const chevronIcon = document.querySelector(".arrow-down");
+  // Add a click event listener to the dropdown toggle
+  dropdownToggle.addEventListener("click", function () {
+    if (dropdownContent.style.display === "flex") {
+      // If content is visible, hide it and change the icon
+      dropdownContent.style.display = "none";
+      chevronIcon.classList.remove("fa-chevron-up");
+      chevronIcon.classList.add("fa-chevron-down");
+    } else {
+      // If content is hidden, show it and change the icon
+      dropdownContent.style.display = "flex";
+      chevronIcon.classList.remove("fa-chevron-down");
+      chevronIcon.classList.add("fa-chevron-up");
+    }
+  });
 
-// Add a click event listener to the dropdown toggle
-dropdownToggle.addEventListener("click", function () {
-  if (dropdownContent.style.display === "flex") {
-    // If content is visible, hide it and change the icon
-    dropdownContent.style.display = "none";
-    chevronIcon.classList.remove("fa-chevron-up");
-    chevronIcon.classList.add("fa-chevron-down");
-  } else {
-    // If content is hidden, show it and change the icon
-    dropdownContent.style.display = "flex";
-    chevronIcon.classList.remove("fa-chevron-down");
-    chevronIcon.classList.add("fa-chevron-up");
-  }
-});
